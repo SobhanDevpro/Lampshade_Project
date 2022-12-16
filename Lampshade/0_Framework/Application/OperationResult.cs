75b0b7bbdaaec9c3b89ef6e692917b2219e218e4
Sobhan <95565757+SobhanDevpro@users.noreply.github.com>
2022-12-16 18:04:15 +0330

    Product Category Management Implemented

---
+++ Lampshade/0_Framework/Application/OperationResult.cs
@@ -0,0 +1,25 @@
+﻿namespace _0_Framework.Application
+{
+  public  class OperationResult
+    {
+        public bool IsSucceeded { get; set; }
+        public string Message { get; set; }
+
+        public OperationResult()
+        {
+            IsSucceeded = false;
+        }
+        public OperationResult Succedded(string message = "عملیات با موفقیت انجام شد")
+        {
+            IsSucceeded = true;
+            Message = message;
+            return this;
+        }
+        public OperationResult Failed(string message)
+        {
+            IsSucceeded = false;
+            Message = message;
+            return this;
+        }
+    }
+}
